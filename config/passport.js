6188c94ac53745954e381968696a23f1a95f8aa5
taamshelbayit <taamshelbayit@gmail.com>
2025-03-02 22:20:12 +0200

    google fix

--- config/passport.js
+++ config/passport.js
@@ -35,7 +35,7 @@ module.exports = function(passport) {
       new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: 'https://www.wokeornot.net/auth/google/callback'
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
