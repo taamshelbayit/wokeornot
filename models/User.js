1bdee9d7bba618ae5e144d6ec2d92bcef2748018
taamshelbayit <taamshelbayit@gmail.com>
2025-03-05 16:54:32 +0200

    New chatgpt session + fixing login and nav bar 3

--- models/User.js
+++ models/User.js
@@ -3,42 +3,50 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  googleId: {
+    type: String
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    // only required if googleId is not present
+    required: function() {
+      return !this.googleId;
+    }
+  },
+  role: {
+    type: String,
+    default: 'user'
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  // etc. (badges, watchers, etc.)
+}, { timestamps: true });
 
-  verified: { type: Boolean, default: false },
-  verifyToken: String,
-  verifyExpires: Date,
-
-  role: { type: String, default: 'user' }, // 'user', 'admin', 'banned'
-
-  badges: [{ type: String }], // e.g. '10-reviews', '50-reviews', etc.
-
-  // For social/follow system
-  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-
-  // For watchlists
-  watchlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-
-  // For advanced search saved queries
-  savedSearches: [{
-    name: String,
-    query: String,
-    createdAt: { type: Date, default: Date.now }
-  }],
-
-  createdAt: { type: Date, default: Date.now }
-});
-
-// Hash password if changed
+// If password is modified and not empty, hash it
 UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+  if (!this.isModified('password') || !this.password) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
